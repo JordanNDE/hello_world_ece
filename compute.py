3c1be74f8a5e6f65391637977142ab497a8a0955
JordanNDE <ndetcho.jordan@gmail.com>
2023-10-11 01:20:27 +0200

    Update compute.py

--- compute.py
+++ compute.py
@@ -1,13 +1,10 @@
 # math_operations.py
 
-def add(x, y):
-    return x + y
+def add(x, y): return x + y
 
-def subtract(x, y):
-    return x - y
+def subtract(x, y): return x - y
 
-def multiply(x, y):
-    return x * y
+def multiply(x, y): return x * y
 
 def divide(x, y):
     if y == 0:
