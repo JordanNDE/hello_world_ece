e2da7095bd587a9a9ee033b55473577385392b72
Jordan Ndetcho <jordan.ndetcho.ext@suez.com>
2023-10-11 17:51:57 +0200

    lint compute

--- compute.py
+++ compute.py
@@ -1,13 +1,27 @@
 # math_operations.py
 
-def add(x, y): return x + y
+def add(x, y): 
+    """
+    this function bla bla 
+    """
+    return x + y
 
-def subtract(x, y): return x - y
+def subtract(x, y): 
+    """
+    this function bla bla 
+    """
+    return x - y
 
 def multiply(x, y):
+    """
+    this function bla bla 
+    """
     return x * y
 
 def divide(x, y):
+    """
+    this function bla bla 
+    """
     if y == 0:
         raise ValueError("Division by zero is not allowed")
     return x / y
@@ -15,4 +29,7 @@ def divide(x, y):
 ## this comment is wrong
 
 def print_jordan():
+    """
+    this function bla bla 
+    """
     print("jordan")
