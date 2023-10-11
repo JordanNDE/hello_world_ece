ea095d031e7e3c21318c96e126213ad77fbdc076
Jordan Ndetcho <jordan.ndetcho.ext@suez.com>
2023-10-11 17:39:11 +0200

    add commetns wrong

--- compute.py
+++ compute.py
@@ -1,10 +1,8 @@
 # math_operations.py
 
-def add(x, y):
-    return x + y
+def add(x, y): return x + y
 
-def subtract(x, y):
-    return x - y
+def subtract(x, y): return x - y
 
 def multiply(x, y):
     return x * y
@@ -14,6 +12,7 @@ def divide(x, y):
         raise ValueError("Division by zero is not allowed")
     return x / y
 
+## this comment is wrong
 
 def print_jordan():
     print("jordan")
