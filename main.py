505a8794cf64be7650b6959de76f204e0fb572c4
JordanNDE <ndetcho.jordan@gmail.com>
2023-12-13 23:49:07 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from fibonacci_project.fibonacci import generate_fibonacci  
+
+if __name__ == "__main__":     
+	n = 10  # Changer la valeur si nécessaire  
+	result = generate_fibonacci(n)     
+	print(f"Fibonacci sequence of {n} elements: {result}") 
